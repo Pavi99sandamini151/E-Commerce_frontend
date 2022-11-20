b6e64d77750dcd275cb77c6ec0427f0171debd89
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-11-20 12:00:33 +0530

    second commit

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styles: ["div{margin: 10px 0px;padding:10px 20px;background-color: #DAD7A0;text-align: center;}"]
+})
+export class NotificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
